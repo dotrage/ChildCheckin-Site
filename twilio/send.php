48837c7b3119bb703b9359b1ddc8c588a728a41d
Chris Ennis <chris@dotrage.com>
2011-04-17 13:13:15 -0600

    test twilio

--- twilio/send.php
+++ twilio/send.php
@@ -40,6 +40,80 @@
 		error_log(http_build_query($result));				
 	}	
 	
+	/*
+	 * 
+	 * array(
+	"student_id" => 1,
+	"fname" => "Sid",
+	"lname" => "Smith",
+	"cname" => "English",
+	"phone" => "6154158558"
+),
+array(
+	"student_id" => 3,
+	"fname" => "Matthew",
+	"lname" => "Rogers",
+	"cname" => "English",
+	"phone" => "6153979595"
+),
+array(
+	"student_id" => 4,
+	"fname" => "Robert",
+	"lname" => "Hartline",
+	"cname" => "English",
+	"phone" => "6153946699"
+),
+array(
+	"student_id" => 6,
+	"fname" => "Justin",
+	"lname" => "Davis",
+	"cname" => "English",
+	"phone" => "8658052410"
+),
+array(
+	"student_id" => 8,
+	"fname" => "Shane",
+	"lname" => "Stenner",
+	"cname" => "English",
+	"phone" => "4109638130"
+),
+array(
+	"student_id" => 10,
+	"fname" => "Nicholas ",
+	"lname" => "Holland",
+	"cname" => "English",
+	"phone" => "6154225205"
+),
+array(
+	"student_id" => 13,
+	"fname" => "Michael",
+	"lname" => "Summar",
+	"cname" => "English",
+	"phone" => "6152607704"
+),
+array(
+	"student_id" => 16,
+	"fname" => "Matt",
+	"lname" => "Kenigson",
+	"cname" => "English",
+	"phone" => "6156244040"
+),
+array(
+	"student_id" => 19,
+	"fname" => "Jake",
+	"lname" => "Robinson",
+	"cname" => "English",
+	"phone" => "6154245019"
+),
+array(
+	"student_id" => 21,
+	"fname" => "Ben",
+	"lname" => "Stucki",
+	"cname" => "English",
+	"phone" => "6159732406"
+),
+	 */
+	
 	if (!empty($_POST['send'])){
 		$array = array(
 			array(
@@ -47,33 +121,42 @@
 				"fname" => "Yvette",
 				"lname" => "",
 				"cname" => "English",
-				"phone" => "6154823299"
+				"phone" => "6154823299",
+				"type" => "sms"
 			),
 			array(
 				"student_id" => "1",
 				"fname" => "Tiffany",
 				"lname" => "",
 				"cname" => "English",
-				"phone" => "6154737742"
+				"phone" => "6154737742",
+				"type" => "voice"
 			),			
 			array(
 				"student_id" => "3",
 				"fname" => "Phil",
 				"lname" => "",
 				"cname" => "English",
-				"phone" => "6155040239"
+				"phone" => "6155040239",
+				"type" => "sms"
 			),	
 			array(
 				"student_id" => "4",
 				"fname" => "Chris",
 				"lname" => "",
 				"cname" => "English",
-				"phone" => "6153648615"
+				"phone" => "6153648615",
+				"type" => "voice"
 			)
 		);
 		
 		foreach ($array as $student){
-			send_voice($student);
+			if ($student['type'] == "voice"){
+				send_voice($student);
+			}
+			if ($student['type'] == "sms"){
+				send_sms($student);
+			}			
 		}
 		
 		//send_sms(array("to"=>"615-364-8615","fname"=>"Chris","cname"=>"Freshman English","student_id"=>"32"));
